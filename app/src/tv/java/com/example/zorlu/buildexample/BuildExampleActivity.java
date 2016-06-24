9a8d3121f0eef9b023187fc7cae33f7d6869287b
amerty95 <amerty95@hotmail.com>
2016-06-24 10:40:17 +0300

    first commit

---
+++ app/src/tv/java/com/example/zorlu/buildexample/BuildExampleActivity.java
@@ -0,0 +1,17 @@
+package com.example.zorlu.buildexample;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
+
+public class BuildExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_build_example);
+        RelativeLayout myLayout = (RelativeLayout) findViewById(R.id.layout);
+      //  myLayout.setBackgroundColor(Color.BLUE);
+    }
+}
